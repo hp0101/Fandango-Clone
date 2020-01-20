14ce29e01a891974b109c14fdbdd9604ab86fb95
Hung Phan <17309881+hp0101@users.noreply.github.com>
2020-01-20 14:19:40 -0800

    added logic

---
+++ Fandango Clone/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Fandango Clone
+//
+//  Created by Hung Phan on 1/20/20.
+//  Copyright © 2020 Hung Phan. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleCell: UILabel!
+    @IBOutlet weak var imageCell: UIImageView!
+    @IBOutlet weak var descriptionCell: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
