0a161c7b238d7e923a31c5d7f27c150e45ff5843
Hung Phan <17309881+hp0101@users.noreply.github.com>
2020-01-20 12:21:27 -0800

    first commit

---
+++ Fandango Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fandango Clone
+//
+//  Created by Hung Phan on 1/20/20.
+//  Copyright © 2020 Hung Phan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Fandango CloneTests/Fandango_CloneTests.swift
@@ -0,0 +1,34 @@
+//
+//  Fandango_CloneTests.swift
+//  Fandango CloneTests
+//
+//  Created by Hung Phan on 1/20/20.
+//  Copyright © 2020 Hung Phan. All rights reserved.
+//
+
+import XCTest
+@testable import Fandango_Clone
+
+class Fandango_CloneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
